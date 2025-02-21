c31fddb52d8e78388cd74371beffdc8c2db5060e
Pedro Linero <xplinari721@ieshnosmachado.org>
2025-02-21 10:36:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// Importar libreria para manejo de ficheros de configuración
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'development'}` 
+});
+// Importar fichero de configuración
+const config = require("./config/config");
+// Importar librería express --> web server
+const express = require("express");
+// Importar librería path, para manejar rutas de ficheros en el servidor
+const path = require("path");
+// Importar libreria CORS
+const cors = require("cors");
+// Importar gestores de rutas
+const obraRoutes = require("./routes/obraRoutes");
+const artistaRoutes = require("./routes/artistaRoutes");
+// const pedidoRoutes = require("./routes/pedidoRoutes");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configurar middleware para analizar JSON en las solicitudes
+app.use(express.json());
+// Configurar CORS para admitir cualquier origen
+// app.use(cors());
+
+// Configurar rutas de la API Rest
+app.use("/api/obras", obraRoutes);
+app.use("/api/artistas", artistaRoutes);
+
+// Configurar el middleware para servir archivos estáticos desde el directorio 'public\old_js_vainilla'
+app.use(express.static(path.join(__dirname, "public")));
+
+//Ruta para manejar las solicitudes al archivo index.html
+// app.get('/', (req, res) => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// // Configurar el middleware para servir archivos estáticos desde el directorio 'public\old_js_vainilla'
+// app.use(express.static(path.join(__dirname, "public","old_js_vainilla")));
+
+// Ruta para manejar las solicitudes al archivo index.html
+// app.get('/', (req, res) => {
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "old_js_vainilla","index.html"));
+// });
+
+// Iniciar el servidor solo si no estamos en modo de prueba
+if (process.env.NODE_ENV !== "test") {
+  app.listen(config.port, () => {
+  console.log(`Servidor escuchando en el puerto ${config.port}`);
+  });
+  }
+  // Exportamos la aplicación para poder hacer pruebas
+  module.exports = app;
